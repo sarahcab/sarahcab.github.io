ade5b60f940ad07060d012106cebdbce6bcc8f7c
sarahcab <sarahcabarry@hotmail.fr>
2018-02-17 14:46:39 +0100

    via github.com

--- diplo/palestine_v6/js/script.js
+++ diplo/palestine_v6/js/script.js
@@ -5,7 +5,7 @@
 var scrollTime=0,
 
 //vue
-vbH=600,
+vbH=581,
 vbW=1190.5,
 vbY=50,
 vbX=[0,30,60,90,10,-30],
@@ -26,9 +26,10 @@ deplace2_rond=false,
 // echell=31.9,
 echell=28.2,
 
+
 //dynamique
 lengthOp=175,
-vit=6;
+vit=8;
 
 //horloge
 ctr_hor=[452.9,245.9],
@@ -36,19 +37,28 @@ heure=6.5,
 H=6.5,
 palliers=[6.5,6.5,7.5,9.25,(9*1+35/60),10.5,11.5],
 px_h=1174.582214,
-bloc_debut=[0,0,1.400295568,0,0.10883186,0.5740891] //temps de blocage par début de trajet (h) -> voir fichier xlsx 'trajets'
+bloc_debut=[0,0,1.400295568,0,0.10883186,0.5740891] //temps de blocage par début de trajet(h) -> voir fichier xlsx 'trajets'
 bloc_fin=[0,0.775980515,0.247110983,0,0.616713871,0.101309841]//temps de blocage par fin de trajet(h) -> voir fichier xlsx 'trajets'
 
 ///Elements de la carte
 parts_legende = ["routes","mur","postes"],
-flechee=false;
+flechee=false,
+
+///General
+tactile=false;
 
 ///        ------        fonctions       ------        ///
 
 ///général
 window.onload = initialize();
 function initialize(){
-	//adapter à la configuration W/H
+	
+	//test si c'est un écran tactile;
+	tactile = is_touch_device();
+	console.log("tactile "+tactile);
+	if(tactile==false){
+		adapt();
+	}
 	window.onresize=function(){
 		resize();
 	}
@@ -57,7 +67,18 @@ function initialize(){
 	demarrer();
 }
 
+
 ///fonctions au démarrage
+//test_tactile
+function is_touch_device(){
+	try{
+		document.createEvent("TouchEvent");
+		return true;
+	} catch(e) {
+		return false
+	}
+}
+
 //affichage
 function resize(){
 	//blocage du scroll
@@ -73,12 +94,15 @@ function resize(){
 	
 	//adaptation de la taille des textes <html> en fonction de la largeur de la page
 	widthPop = document.getElementById("dessins").offsetWidth;
-	var val = widthPop/126.8;
+	var val = widthPop/100;
 	var val2 = widthPop/63;
 	d3.selectAll(".src")
 		.style("font-size",val+"px")
 	d3.select("#indication_debut")
 		.style("font-size",val2+"px")
+	d3.select("#lab_vite")
+		.style("font-size",val+"px")
+	
 		
 }
 
@@ -95,7 +119,7 @@ function demarrer(){
 				zooms(); //évènements au clic - zoom
 				
 				//deuxième indication de scroll
-				d3.select("#fleche").transition().delay(2000).duration(650).attr("transform","translate(0,450)").transition().attr("class","clignote")
+				d3.select("#fleche").transition().delay(2000).duration(650).attr("transform","translate(0,480)").transition().attr("class","clignote")
 				d3.select("#cible_caisson").transition().delay(2000).attr("class","clignote").transition().delay(5000).attr("class","")
 				d3.select("#boutons").transition().delay(2000).attr("class","clignote").transition().delay(5000).attr("class","")
 				
@@ -129,6 +153,10 @@ function demarrer(){
 		})
 }
 
+function adapt(){
+	d3.select("#inner_range").style("display","none");
+	
+}
 ///implementation des variables globales (recherche des chemins)
 function variables(){
 	for(i=1;i<6;i++){
@@ -175,38 +203,61 @@ function scrollable(){
 	//fonctionnement des évènements au scroll
 	scrollTime=1;  //variable d'étape par défaut
 	way=lengthOp*0.75;  //variable du trajet par défaut
-	window.addEventListener("scroll", function (e){
-		console.log("s");
-		scrollTo(0,0);
-		scrollanim2(e);
-	}, false);
+	
+	if(tactile==true){
+		window.addEventListener("wheel", function (e){
+			scrollanim2(e.deltaY);
+		}, false);
+	} else {
+		alert("drag!")
+		var drag = d3.behavior.drag() 
+			.on("drag", function() {
+				scrollanim2(d3.event.dy);
+			})
+		d3.select("#svg_fix")//.data([{"x":0}])
+			.call(drag)
+	}
 }
 
 ///évènements au clic - boutons
 function boutons(){	
 	
+	//vitesse
+	document.getElementById("vitesse").value=vit;
+	d3.select("#inner_range").style("display","block");
+	document.getElementById("vitesse").addEventListener("change", function (e){
+		vit=this.value;
+	}, false);
+	
 	//afficher/masquer les éléments de la légende
 	for(i=0;i<parts_legende.length;i++){
-		nom=parts_legende[i];
 		
-		d3.select("#tout_"+nom)
-			.each(function(){
-				d3.select(this).attr("lock","false")
-				d3.selectAll(".elements_"+nom).attr("opacity",1).transition().duration(1400).attr("opacity",0)
+		//paramètres par défaut
+		nom=parts_legende[i];
+		d3.select("#legende_"+nom)
+			.attr("opacity",1)
+			.transition()
+			.duration(1400)
+			.attr("opacity",0.4)
+		
+		d3.select("#tout_"+nom).attr("lock","false")
+		d3.selectAll(".elements_"+nom).attr("opacity",1).transition().duration(1400).attr("opacity",0)
 
-				d3.select(this).select(".bou")
-					.transition()
-					.duration(1400)
-					.attr("transform","translate(-17,0)")
-				d3.select(this)
-					.transition()
-					.delay(1000)
-					.selectAll(".ti")
-					.attr("fill",function(){
-						var val = this.attributes.fill_off.value;
-						return val;
-					})					
-			})
+		d3.select("#tout_"+nom).select(".bou")
+			.transition()
+			.duration(1400)
+			.attr("transform","translate(-17,0)")
+		d3.select("#tout_"+nom)
+			.transition()
+			.delay(1000)
+			.selectAll(".ti")
+			.attr("fill",function(){
+				var val = this.attributes.fill_off.value;
+				return val;
+			})	
+		
+		//actions
+		d3.select("#tout_"+nom)
 			.style("cursor","pointer")
 			.on("click",function(){
 				nom = (this.id).split("_")[1];
@@ -251,8 +302,15 @@ function boutons(){
 			})
 			.on("mouseout",function(){
 				nom = (this.id).split("_")[1];
+				tst = this.attributes.lock.value;
 				d3.select("#legende_"+nom)
-					.attr("opacity",1)
+					.attr("opacity",function(){
+						if(tst=="false"){
+							return 0.4
+						} else {
+							return 1
+						}
+					})
 			})
 	
 	}
@@ -291,54 +349,6 @@ function boutons(){
 
 ///évènements au clic - zooms
 function zooms(){
-	
-	//sources - agrandie par défaut
-	// d3.select("#sources_tout")
-		// .style("cursor","zoom-out")
-		// .attr("zm","in")
-		// .on("click",function(){
-			// d3.select("#cible_caisson").attr("class","")
-				
-			// zm = this.attributes.zm.value;
-			// if(zm=="out"){
-				// d3.select(this)
-					// .attr("zm","in")
-					// .style("cursor","zoom-out")
-				
-				// d3.select("#keffieh")
-					// .transition()
-					// .duration(700)
-					// .attr("transform","scale(2.6) translate(-588,100)")
-				
-				// d3.select("#sources_total")
-					// .transition()
-					// .duration(700)
-					// .attr("opacity",1)
-				// d3.select("#sources_icone")
-					// .transition()
-					// .duration(700)
-					// .attr("opacity",0)
-				
-			// } else {
-				// d3.select(this)
-					// .attr("zm","out")
-					// .style("cursor","zoom-in")
-				
-				// d3.select("#keffieh")
-					// .transition()
-					// .duration(700)
-					// .attr("transform","translate(130,550)")
-				
-				// d3.select("#sources_total")
-					// .transition()
-					// .duration(700)
-					// .attr("opacity",0)
-				// d3.select("#sources_icone")
-					// .transition()
-					// .duration(700)
-					// .attr("opacity",1)
-			// }
-		// })
 		
 	//caisson - réduit par défaut
 	d3.select("#tout_caisson")
@@ -377,17 +387,15 @@ function zooms(){
 }
 
 ///fonctionnement des évènements au scroll
-function scrollanim2(e){
+function scrollanim2(delta){
 	if(flechee==false){
 		flechee=true;
-		d3.select("#fleche").attr("class","").transition().duration(700).attr("transform","translate(0,900)").transition().remove();
-	
+		d3.select("#fleche").attr("class","").transition().duration(700).attr("transform","translate(0,1500)")//.transition().remove();
 	}
-			
 	var L=pl_traces[scrollTime];
-	
+	console.log("delta"+delta);
 	///ALLER
-	if(e.deltaY>0&&av==true){
+	if(delta>0&&av==true){
 		if(scrollTime>5&&(way>=L||way==0)){
 			d3.select("body").attr("class","monter")
 		} else {
@@ -399,12 +407,10 @@ function scrollanim2(e){
 			if(way<lengthOp){
 				d3.select("#k_"+(scrollTime-1)).attr("opacity",function(){
 					op = way/lengthOp;
-					console.log("op : "+op);
 					if (op>1){
 						op=1;
 					}
 					op=Math.sqrt(1-op);
-					console.log("opok : "+op);
 					return op;
 				})
 				d3.select("#info_tous")
@@ -435,12 +441,12 @@ function scrollanim2(e){
 				d3.select("#trace_"+scrollTime).attr("stroke-dasharray",(way-lengthOp)+","+(L-way+lengthOp*1)).attr("opacity",1).attr("stroke","#92C020")//.attr("stroke","#0_0A055")//.attr("stroke","#4_8927F")
 				
 				//km
-				way_km = way_km*1+vit*1;
+				way_km = way_km*1+vit*1
 				km=Math.trunc(way_km/echell);
 
 					
 				// maj_heures
-				heure=heure*1+vit/px_h;
+				heure=heure*1+vit*1/px_h;
 				d3.select("#nbkm").text(km).attr("transform",function(){
 					if(km<10){
 						return "matrix(1 0 0 1 304.0847 266.6378)"
@@ -459,7 +465,7 @@ function scrollanim2(e){
 				d3.select("#roue").attr("opacity",0.5) 
 					.attr("transform",function(){
 						var r = ((this.attributes.transform.value).split("(")[1]).split(" ")[0];
-						return "rotate("+(r*1+vit*0.5)+" 98.4 98.4)"
+						return "rotate("+(r*1+vit*1*0.5)+" 98.4 98.4)"
 					})
 				
 			} else if(way<L){
@@ -531,9 +537,7 @@ function scrollanim2(e){
 					if (op2>1){
 						op2=1;
 					}
-					console.log("op2 "+(1-op2))
 					op2=Math.sqrt(1-op2);
-					console.log("ok2 "+op2)
 					return op2;
 				})
 				d3.select("#info_tous").attr("opacity",op2)
@@ -579,7 +583,7 @@ function scrollanim2(e){
 			d3.select("body").attr("class","deux_sens")
 			op=0;
 			op2=0;
-			way=way-vit;
+			way=way*1-vit*1;
 			
 			if(way>L-lengthOp){
 				d3.select("#k_"+scrollTime).attr("opacity",function(){
@@ -620,7 +624,7 @@ function scrollanim2(e){
 				d3.select("#contour"+(scrollTime-1)+"_plus").attr("display","block")
 				d3.select("#contour"+scrollTime).attr("display","none")
 				d3.select("#contour"+scrollTime+"_plus").attr("display","none")
-				
+				d3.select("#lum_pt"+(scrollTime)+"_plus").attr("opacity",0)
 				
 				d3.select("#info_tous").attr("opacity",0)
 				d3.select("#legende_etape").attr("opacity",0)
@@ -630,14 +634,14 @@ function scrollanim2(e){
 				d3.select("#roue").attr("opacity",0.5)
 					.attr("transform",function(){
 						var r = ((this.attributes.transform.value).split("(")[1]).split(" ")[0];
-						return "rotate("+(r*1-vit*0.5)+" 98.4 98.4)"
+						return "rotate("+(r*1-vit*1*0.5)+" 98.4 98.4)"
 					})
 					
 				//km
 				way_km = way_km*1-vit*1;
 				km=Math.trunc(way_km/echell);
 				
-				heure=heure*1-vit/px_h;
+				heure=heure*1-vit*1/px_h;
 				// H=heure;
 				d3.select("#nbkm").text(km).attr("transform",function(){
 					if(km<10){
@@ -678,12 +682,9 @@ function scrollanim2(e){
 				d3.select("#petite_aiguille").attr("transform","rotate("+angle_h+" "+ctr_hor[0]+" "+ctr_hor[1]+")")
 				//
 				
-				d3.select("#etape_"+scrollTime).attr("opacity",op)
+				d3.select("#etape_"+scrollTime).attr("opacity",0)
 				d3.select("#etape_"+scrollTime).selectAll("circle")
-					.attr("r",function(){
-						r = this.attributes.r0.value;
-						return r*op;
-					})
+					.attr("r",0)
 				
 				if(deplace2==false&&document.getElementById("point_"+scrollTime)){ //retour
 					var Y0=document.getElementById("point_"+(scrollTime-1)).attributes.cy.value;
