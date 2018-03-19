44f908ccbfbd000c16aeda465416b350f329bb7c
sarahcab <sarahcabarry@hotmail.fr>
2018-03-19 20:13:47 +0100

    Update blop.py

--- blop.py
+++ blop.py
@@ -1,24 +1,2 @@
-scroll
-bubbles: true
-cancelBubble: false
-cancelable: true
-composed: false
-currentTarget: null
-defaultPrevented: false
-detail: 0
-eventPhase: 0
-explicitOriginalTarget: HTMLDocument file:///C:/Users/Sarah/Documents/diplo/palestine/palestine_interactif_v1/index.html
-isTrusted: true
-layerX: 0
-layerY: 0
-originalTarget: HTMLDocument file:///C:/Users/Sarah/Documents/diplo/palestine/palestine_interactif_v1/index.html
-pageX: 0
-pageY: 5
-rangeOffset: 0
-rangeParent: null
-target: HTMLDocument file:///C:/Users/Sarah/Documents/diplo/palestine/palestine_interactif_v1/index.html
-timeStamp: 56804.122161937354
-type: "scroll"
-view: Window file:///C:/Users/Sarah/Documents/diplo/palestine/palestine_interactif_v1/index.html
-which: 0
-__proto__: UIEventPrototype { initUIEvent: initUIEvent(), view: Getter, detail: Getter, … }
+<li id="amSiteA" class="elmt" lab="Projet(s) d'aménagement en cours sur le site :" position="avant" type="booleen">Projet(s) d'aménagement en cours sur le site :</li>
+<p id="amChemA_D" class="elmt" lab="" position="avant" type="in_bool" style="display: none;"></p>
