fd3fcd5209dd427134a0e0e08923af8b787e8625
sarahcab <sarahcabarry@hotmail.fr>
2018-03-19 20:14:07 +0100

    click1

--- blop.py
+++ blop.py
@@ -1,2 +1,2 @@
 <li id="amSiteA" class="elmt" lab="Projet(s) d'aménagement en cours sur le site :" position="avant" type="booleen">Projet(s) d'aménagement en cours sur le site :</li>
-<p id="amChemA_D" class="elmt" lab="" position="avant" type="in_bool" style="display: none;"></p>
+<p id="amChemA_D" class="elmt" lab="" position="avant" type="in_bool" style="">Dede</p>
